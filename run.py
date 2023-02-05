665d97afaeb09175497644c30f8c6e54422c8714
Bojan Bakaric <bakaricbojan@gmail.com>
2023-02-05 16:11:54 +0000

    Wire up APIs and test access to worksheet

---
+++ run.py
@@ -0,0 +1,23 @@
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('space_quiz')
+
+questions = SHEET.worksheet('questions')
+question = questions.get_all_values()
+
+answers = SHEET.worksheet('answers')
+answer = answers.get_all_values()
+
+print(question[0][0])
+print(answer[0][0])
