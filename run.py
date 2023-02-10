a6293354f4d2f54389827be671628a9e8f37c519
Bojan Bakaric <bakaricbojan@gmail.com>
2023-02-10 14:40:06 +0000

    Add player's name input and relevant conditions

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-
+# Import necessary modules
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -13,11 +13,30 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('space_quiz')
 
-questions = SHEET.worksheet('questions')
-question = questions.get_all_values()
+quest = SHEET.worksheet('questions')
+question = quest.get_all_values()
 
-answers = SHEET.worksheet('answers')
-answer = answers.get_all_values()
+answ = SHEET.worksheet('answers')
+answer = answ.get_all_values()
 
-print(question[0][0])
-print(answer[0][0])
+
+print('ASCII ART, intro text')
+
+"""
+Prompt the user to enter their name and validate the input.
+The input must only contain letters and/or numbers and
+have a minimum length of 2 characters.The loop continues until a
+valid name is entered, breaking the loop and printing the 
+player's name to the terminal.
+"""
+while True:
+    player = input('Please enter your name:').capitalize()
+    if not player.isalnum():
+        print('You must enter a name using only letters and numbers!')
+        continue
+    elif len(player) < 2:
+        print('You have to use a minimum of 2 letters and/or numbers.')
+        continue
+    else:
+        print(f'Hello {player}!')
+        break
